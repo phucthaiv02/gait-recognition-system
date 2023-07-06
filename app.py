de197f1555cfa38bcf9d628d45cf8df7c39258a2
De Pie <phucthaiv02@gmail.com>
2023-07-06 17:18:53 +0700

    Initial css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
@@ -8,13 +8,20 @@ def home():
     return render_template('index.html')
 
 
-# Cho phép phương thức POST
 @app.route('/', methods=['GET', 'POST'])
 def upload():
     video = request.files['video']
     filename = 'videos/' + video.filename
     video.save('static/videos/' + video.filename)
     return render_template("index.html", video_name=filename)
+
+
+@app.route('/download')
+def download_file():
+    file_path = 'static\\videos\\probe4 (1).mp4'
+    file_name = ''
+
+    return send_file(file_path, as_attachment=True)
 
 
 if __name__ == '__main__':
