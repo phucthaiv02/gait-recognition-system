e074cdad19557dca3189ba5a2776dc25580d4dee
De Pie <phucthaiv02@gmail.com>
2023-07-07 11:12:14 +0700

    Create upload data page, config css file

--- app.py
+++ app.py
@@ -1,19 +1,55 @@
 from flask import Flask, render_template, request, send_file
+import json
 
 app = Flask(__name__)
+
+CURRENT_VIDEO = ""
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', image_name='images/bg.png')
 
 
-@app.route('/', methods=['GET', 'POST'])
-def upload():
-    video = request.files['video']
-    filename = 'videos/' + video.filename
-    video.save('static/videos/' + video.filename)
-    return render_template("index.html", video_name=filename)
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    def process():
+        if 'video' in request.files:
+            video = request.files['video']
+            video_name = 'videos/' + video.filename
+
+            ##
+            # Chạy hàm xử lý video tại đây
+            ##
+
+            video.save('static/videos/' + video.filename)
+            data_files = ['static/json/data1.json', 'static/json/data2.json',
+                          'static/json/data3.json']
+            return video_name, data_files
+        else:
+            return "", []
+
+    video_name, data_files = process()
+    objects = []
+    for file in data_files:
+        with open(file) as f:
+            data = json.load(f)
+        objects.append(data)
+    if video_name != "":
+        if objects != []:
+            return render_template('index.html', video_name=video_name, objects=objects)
+        return render_template('index.html', video_name=video_name)
+    else:
+        return render_template("index.html", image_name='images/bg.png')
+
+
+@app.route('/upload', methods=['POST', 'GET'])
+def add_data():
+    if 'video' in request.files and 'name' in request.form:
+        push = True
+        return render_template('upload.html', push=push)
+    else:
+        return render_template('upload.html')
 
 
 @app.route('/download')
